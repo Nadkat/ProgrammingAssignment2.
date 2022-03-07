7aa3f6c82b88a336d43c1a530834160a68b28d45
Nadkat <75786881+Nadkat@users.noreply.github.com>
2022-03-07 20:01:27 +0100

    Add files via upload

---
+++ ProgrammingAssignment2.R
@@ -0,0 +1,27 @@
+
+makeCacheMatrix <- function(x = matrix()) {
+     inv <- NULL
+     set <- function(y) {
+           x <<- y
+           inv <<- NULL
+       }
+     get <- function() x
+     setInverse <- function(inverse) inv <<- inverse
+     getInverse <- function() inv
+  list(set = set,
+                  get = get,
+                  setInverse = setInverse,
+                  getInverse = getInverse)
+ }
+cacheSolve <- function(x, ...) {
+     j <- x$getInverse()
+     if(!is.null(j)){
+           message("getting cached data")
+           return(j)
+     }
+     mat <- x$get()
+     j <- solve(mat,...)
+     x$setInverse(j)
+     j
+}
+
